cb9e7e894eb0860e14b3bab8460ae830bd6b6875
Josu775 <josu.saezdegordoa@opendeusto.es>
2025-12-14 12:30:10 +0100

    asd

--- gestion/admin.py
+++ gestion/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
 from .models import Agente, Cliente, Propiedad
 
-admin.site.register(Agente)
-admin.site.register(Cliente)
-admin.site.register(Propiedad)
+# ADMIN DE AGENTE
+@admin.register(Agente)
+class AgenteAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "email", "telefono")
+    search_fields = ("nombre", "email")
+    list_filter = ("telefono",)
+    ordering = ("nombre",)
+
+# ADMIN DE CLIENTE
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "email", "telefono")
+    search_fields = ("nombre", "email")
+    list_filter = ("telefono",)
+    ordering = ("nombre",)
+
+# ADMIN DE PROPIEDAD
+@admin.register(Propiedad)
+class PropiedadAdmin(admin.ModelAdmin):
+    list_display = ("direccion", "precio", "agente", "cliente")
+    search_fields = ("direccion", "descripcion")
+    list_filter = ("agente", "cliente", "precio")
+    ordering = ("precio",)
+
+    readonly_fields = ("precio",)
